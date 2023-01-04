b8bb769b77fba841d4cfdbd6fc36d4993749cea1
npham49 <brian.d.pham@gov.bc.ca>
2023-01-03 21:18:50 -0800

    added functionality with the new API

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -7,8 +7,8 @@ const ProtectedRoute = ({ children,role }:{ children: JSX.Element,role:string })
   useEffect(()=> {
     async function checkRole() {
       const token = await getIdTokenClaims()
-      console.log(token?.[window.location.origin + '/roles'])
-      console.log(window.location.origin)
+      // console.log(token?.[window.location.origin + '/roles'])
+      // console.log(window.location.origin)
     }
     checkRole()
   },[])
--- src/pages/Shared.tsx
+++ src/pages/Shared.tsx
@@ -1,5 +1,7 @@
-import {useEffect, useState} from 'react'
-import AWS from 'aws-sdk'
+import { useEffect, useState } from "react";
+import AWS from "aws-sdk";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
 AWS.config.update({
   accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
@@ -11,22 +13,25 @@ const s3 = new AWS.S3();
 
 const params = {
   Bucket: import.meta.env.VITE_S3_BUCKET_NAME,
-  Delimiter: '',
-  Prefix: 'sharedspace/',
+  Delimiter: "",
+  Prefix: "sharedspace/",
 };
 
 const Shared = () => {
-  const [fileList, setfileList] = useState<any[]>([])
+  const [fileList, setfileList] = useState<any[]>([]);
+  const [accessToken, setAccessToken] = useState<string>("");
+
+  const { getAccessTokenSilently } = useAuth0();
 
   //Get a temporary access key for 60 seconds
   const getUrl = (key: string) => {
-    const url = s3.getSignedUrl('getObject', {
+    const url = s3.getSignedUrl("getObject", {
       Bucket: import.meta.env.VITE_S3_BUCKET_NAME,
       Key: key,
-      Expires: 60 ,
-    })
-    return url
-  }
+      Expires: 60,
+    });
+    return url;
+  };
 
   useEffect(() => {
     // s3.getObject({
@@ -39,18 +44,49 @@ const Shared = () => {
     //     console.log(data);
     //   }
     // });
-    
+
+    const getAccessToken = async () => {
+      try {
+        const token = await getAccessTokenSilently(
+          {
+            audience: `${import.meta.env.VITE_API_URL}/getfiles`,
+            scope: "read:getfiles",
+          }
+        );
+        setAccessToken(token);
+        console.log(token);
+        
+      } catch (e: any) {
+        console.log(e.message);
+      }
+    };
+    getAccessToken();
+
     s3.listObjectsV2(params, (err, data) => {
       if (err) {
         console.log(err, err.stack);
       } else {
-        console.log(data.Contents);
-        setfileList(data.Contents!)
+        // console.log(data.Contents);
+        setfileList(data.Contents!);
       }
     });
-  // console.log(fileList);
-  }, [])
-  
+    // console.log(fileList);
+  }, []);
+
+  useEffect(() => {
+    console.log("there is a token")
+    if (accessToken) {
+      const config = {
+        headers: { 
+          "Content-Type": "application/json",Authorization: `Bearer ${accessToken}` },
+      };
+      axios
+        .get(`${import.meta.env.VITE_API_URL}/getfiles`, config)
+        .then(res=> {console.log(res)})
+        .catch(console.log);
+    }
+  }, [accessToken]);
+
   return (
     // A file library to display all the files pulled from s3 bucket, it has key being file name and publicUrl the link to the file
     //the library uses tailwindcss
@@ -59,23 +95,24 @@ const Shared = () => {
       {/* upload button */}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Upload
-        </button>
-      <div className="text-2xl font-bold">Files:</div> 
+      </button>
+      <div className="text-2xl font-bold">Files:</div>
       <div className="flex flex-col items-center justify-center">
-      {fileList.map((file) => (
-        <div key={file.Key} className="flex flex-ocl items-center justify-center underline">
-          <div className="text-xl font-bold">
-            <a href={getUrl(file.Key)}>
-              {file?.Key.replace('sharedspace/', '')}
-            </a>
+        {fileList.map((file) => (
+          <div
+            key={file.Key}
+            className="flex flex-ocl items-center justify-center underline"
+          >
+            <div className="text-xl font-bold">
+              <a href={getUrl(file.Key)}>
+                {file?.Key.replace("sharedspace/", "")}
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
-    
+  );
+};
 
-  )
-}
-
-export default Shared
+export default Shared;
