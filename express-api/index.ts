3369ddd6332c67202c962e77f534af210c483896
npham49 <brian.d.pham@gov.bc.ca>
2023-01-07 21:47:56 -0800

    added file upload functionality to the main sharedspace

--- express-api/index.ts
+++ express-api/index.ts
@@ -5,6 +5,9 @@ import helmet from "helmet";
 import { checkJwt } from "./middleware/auth";
 import bodyParser from 'body-parser';
 import AWS from "aws-sdk";
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { ListObjectsCommand, S3Client } from '@aws-sdk/client-s3';
 
 dotenv.config();
 
@@ -14,13 +17,17 @@ AWS.config.update({
   region: process.env.S3_BUCKET_REGION,
 });
 
-const s3 = new AWS.S3();
 
-const params = {
-  Bucket: process.env.S3_BUCKET_NAME || '',
-  Delimiter: "",
-  Prefix: "sharedspace/",
-};
+
+const s3 = new S3Client({
+  region: process.env.S3_BUCKET_REGION,
+  credentials:{
+     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+     secretAccessKey: process.env.AWS_SECRET_KEY || ''
+ }
+})
+
+
 
 const app = express();
 app.use(helmet());
@@ -31,19 +38,55 @@ app.use(bodyParser.urlencoded({
 }));
 const port = process.env.PORT || 3000;
 
-app.get('/', (req:any, res) => res.send('Hello World!'));
-
-app.get('/getfiles', checkJwt, (req:any, res) => {
+app.get('/files', checkJwt, async (req:any, res) => {
   console.log(req.auth[`${process.env.APP_URL}/roles`]);
   if (req.auth[`${process.env.APP_URL}/roles`].includes('member') || req.auth[`${process.env.APP_URL}/roles`].includes('admin')) {
-    s3.listObjectsV2(params, (err, data) => {
-      if (err) {
-        res.status(400).json(err);
-      } else {
-        // console.log(data.Contents);
-        res.status(200).json(data);
+    const bucketParams = {
+      Bucket: process.env.S3_BUCKET_NAME || '',
+      Delimiter: "",
+      Prefix: `${req.query['folder']}/`,
+    };
+    try {
+      const data = await s3.send(new ListObjectsCommand(bucketParams));
+      // console.log("Success", data);
+      res.status(200).json(data);
+      return // For unit tests.
+    } catch (err) {
+      // console.log("Error", err);
+      res.status(500).json({ error: err });
+    }
+    return
+  }
+  res.status(401).json({ error: "Not Authorized" })
+});
+
+app.post('/files', checkJwt, (req:any, res) => {
+  console.log(req.auth[`${process.env.APP_URL}/roles`]);
+  if (req.auth[`${process.env.APP_URL}/roles`].includes('member') || req.auth[`${process.env.APP_URL}/roles`].includes('admin')) {
+    // res.status(200).json({ message: "File uploaded successfully!" });
+    const path = req.query['folder'];
+    const upload = multer({
+      storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_BUCKET_NAME || '',
+        metadata: function (req, file, cb) {
+          cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+          let newFileName = file.originalname;
+          console.log(file.originalname)
+          let fullPath = `${path}/`+ newFileName;
+          cb(null, fullPath);
+        }
+      })
+    })
+    upload.single('file')(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        console.log(err)
+        res.status(500).json({ error: err });
       }
-    });
+    })
+    res.status(200).json({ message: "File uploaded successfully!" });
     return
   }
   res.status(401).json({ error: "Not Authorized" })
--- src/pages/Shared.tsx
+++ src/pages/Shared.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import AWS from "aws-sdk";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
@@ -20,6 +20,7 @@ const params = {
 const Shared = () => {
   const [fileList, setfileList] = useState<any[]>([]);
   const [accessToken, setAccessToken] = useState<string>("");
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const { getAccessTokenSilently } = useAuth0();
 
@@ -47,45 +48,80 @@ const Shared = () => {
 
     const getAccessToken = async () => {
       try {
-        const token = await getAccessTokenSilently(
-          {
-            audience: `${import.meta.env.VITE_API_URL}/getfiles`,
-            scope: "read:getfiles",
-          }
-        );
+        const token = await getAccessTokenSilently({
+          audience: `${import.meta.env.VITE_API_URL}/getfiles`,
+          scope: "read:getfiles",
+        });
         setAccessToken(token);
         console.log(token);
-        
       } catch (e: any) {
         console.log(e.message);
       }
     };
     getAccessToken();
-
-    s3.listObjectsV2(params, (err, data) => {
-      if (err) {
-        console.log(err, err.stack);
-      } else {
-        // console.log(data.Contents);
-        setfileList(data.Contents!);
-      }
-    });
     // console.log(fileList);
   }, []);
 
   useEffect(() => {
-    console.log("there is a token")
+    console.log("there is a token");
     if (accessToken) {
       const config = {
-        headers: { 
-          "Content-Type": "application/json",Authorization: `Bearer ${accessToken}` },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          folder: "sharedspace",
+        },
       };
       axios
-        .get(`${import.meta.env.VITE_API_URL}/getfiles`, config)
-        .then(res=> {console.log("this is from the API");console.log(res)})
+        .get(`${import.meta.env.VITE_API_URL}/files`, config)
+        .then((res) => {
+          console.log("this is from the API");
+          console.log(res);
+          if (res.status === 200) {
+            setfileList(res.data.Contents);
+          } else {
+            console.log("error");
+          }
+
+        })
         .catch(console.log);
     }
   }, [accessToken]);
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    const data = new FormData();
+    data.append("file", selectedFile!);
+    const fileconfig = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        folder: "sharedspace",
+      },
+    };
+
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/files`, data, fileconfig)
+      .then((res) => {
+        console.log("this is from the API");
+        console.log(res);
+        if (res.status === 200) {
+          window.location.reload();
+        } else {
+          console.log("error");
+        }
+      })
+      .catch(console.log);
+  };
+
+  const handleFileSelect = (event: any) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
   return (
     // A file library to display all the files pulled from s3 bucket, it has key being file name and publicUrl the link to the file
@@ -93,9 +129,16 @@ const Shared = () => {
     <div className="flex flex-col items-center justify-center">
       <div className="text-5xl font-bold">Shared</div>
       {/* upload button */}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Upload
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileSelect} />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Upload
+        </button>
+      </form>
+
       <div className="text-2xl font-bold">Files:</div>
       <div className="flex flex-col items-center justify-center">
         {fileList.map((file) => (
